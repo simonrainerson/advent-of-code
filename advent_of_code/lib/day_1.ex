fd1b532d3dbce7f9919dc5f8b74448a44e8fe565
Simon Rainerson <simonr88@gmail.com>
2023-12-11 01:25:59 +0100

    ⚡ Add part 2 of day 1
    Tricky, since neither the text or the examples clearly stated what to do
    with overlapping digits, like the string oneight. Should it be 1ight or 18?
    Turned out to be the latter.

--- advent_of_code/lib/day_1.ex
+++ advent_of_code/lib/day_1.ex
@@ -1,8 +1,55 @@
 defmodule Day1 do
+  @moduledoc """
+  Day 1, solving coordinate for a trebuchet. Do it by concatenating the first
+  and last digit in a string, then sum all strings.
+  """
+
+  @doc """
+  Take something streamable or read from input for day 1, and calculate the sum
+  of first and last digit for each line.
+  """
   def part1(stream \\ Inputs.stream(1)) do
     stream
     |> Stream.map(&String.replace(&1, ~r/\D/, ""))
     |> Stream.map(&(String.first(&1) <> String.last(&1)))
+    |> Stream.map(&String.to_integer/1)
+    |> Enum.sum()
+  end
+
+  @doc """
+  Return a list of digits in the string, considering named digits.
+  """
+  def to_numbers(letters) do
+    Regex.scan(~r/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/, letters)
+    |> Enum.map(&List.last/1)
+    |> Enum.map(
+      &String.replace(
+        &1,
+        ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"],
+        fn number ->
+          case number do
+            "one" -> "1"
+            "two" -> "2"
+            "three" -> "3"
+            "four" -> "4"
+            "five" -> "5"
+            "six" -> "6"
+            "seven" -> "7"
+            "eight" -> "8"
+            "nine" -> "9"
+          end
+        end
+      )
+    )
+  end
+
+  @doc """
+  Same as part1 but also take named digits (e.g. "one" is "1") into account.
+  """
+  def part2(stream \\ Inputs.stream(1)) do
+    stream
+    |> Stream.map(&to_numbers/1)
+    |> Stream.map(&(List.first(&1) <> List.last(&1)))
     |> Stream.map(&String.to_integer/1)
     |> Enum.sum()
   end
--- advent_of_code/test/day1_test.exs
+++ advent_of_code/test/day1_test.exs
@@ -4,4 +4,26 @@ defmodule Day1Test do
   test "part1/1" do
     assert 142 == Day1.part1(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"])
   end
+
+  describe "day 1 part 2" do
+    test "to_numbers" do
+      assert ["1"] == Day1.to_numbers("one")
+      assert ["1", "2", "3", "4"] == Day1.to_numbers("1two3four")
+      assert ["5"] == Day1.to_numbers("ffffiveeee")
+      assert ["1", "8"] == Day1.to_numbers("oneight")
+    end
+
+    test "part2/1" do
+      assert 281 ==
+               Day1.part2([
+                 "two1nine",
+                 "eightwothree",
+                 "abcone2threexyz",
+                 "xtwone3four",
+                 "4nineeightseven2",
+                 "zoneight234",
+                 "7pqrstsixteen"
+               ])
+    end
+  end
 end
